14724eddc78809478d5fbd2ad6077d91b081349d
Insonia <yinkaize007@gmail.com>
2014-04-02 15:17:53 +0800

    isMatch

---
+++ src/com/leetcode/oj/Solution_141.java
@@ -0,0 +1,15 @@
+package com.leetcode.oj;
+
+public class Solution_141 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static boolean isMatch(String s, String p) {
+        if(s.length() == 0) return p.length() == 0;
+        
+        
+    }
+}
